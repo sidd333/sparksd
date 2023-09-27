cd2a17afa5617bd98faec1e064e19553f279c764
siddhant chettri <siddhantchtri@gmail.com>
2023-09-27 11:17:52 +0530

    -initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="h-screen bg-[url('bgg.jpg')]">
+        {" "}
+        <h2 className="pt-64 text-center text-3xl text-white">
+          This application is fully rendered <b>server side </b>and could take
+          upto a<b> few minutes to load</b>{" "}
+        </h2>
+        <div className="w-fit mx-auto pt-10 ">
+          <a href="https://ww-48uz.onrender.com/">
+            <button className="bg-blue-500 p-2 rounded hover:bg-blue-600">
+              Continue
+            </button>
+          </a>
+        </div>
+      </div>
+    </>
   );
 }
 
